56307b3cc11f31e4cb99efd7167d14790af0d27b
Mauro Delazeri <maurodelazeri@gmail.com>
2019-12-31 17:15:44 -0500

    changes

---
+++ main.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include "indicators.h"
+
+void print_array(const double *p, const int size) {
+    int i;
+    for (i = 0; i < size; ++i) {
+        if (i) printf(", ");
+        printf("%.1f", p[i]);
+    }
+    printf("\n");
+}
+
+
+
+int main() {
+    const double data_in[] = {5,8,12,11,9,8,7,10,11,13};
+    const int input_length = sizeof(data_in) / sizeof(double);
+
+    printf("We have %d bars of input data.\n", input_length);
+    print_array(data_in, input_length);
+
+    const double options[] = {3};
+    printf("Our option array is: ");
+    print_array(options, sizeof(options) / sizeof(double));
+
+    const int start = ti_sma_start(options);
+    printf("The start amount is: %d\n", start);
+
+    const int output_length = input_length - start;
+    auto *data_out = static_cast<double *>(malloc((unsigned int) output_length * sizeof(double)));
+    assert(data_out != nullptr);
+    printf("The output length is: %d\n", output_length);
+
+    const double *all_inputs[] = {data_in};
+    double *all_outputs[] = {data_out};
+    int error = ti_sma(input_length, all_inputs, options, all_outputs);
+    assert(error == TI_OKAY);
+
+
+    printf("The output data is: ");
+    print_array(data_out, output_length);
+
+
+    return 0;
+}
